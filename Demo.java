ad9dfd94dbe4ae95d951ef5969d64726cd9729c8
Chaitanya Swaroop Kumar Allu <chaitanyaa1999@gmail.com>
2023-02-04 17:12:45 +0530

    Changes made in Demo.java added comments

--- Demo.java
+++ Demo.java
@@ -1,5 +1,6 @@
 class Demo{
-	public static void main(String args[]) throws IOException{
-		System.out.println("Hello World");
+	public static void main(String args[]){
+		// Prints "Hello World (Demo)" message
+		System.out.println("Hello World (Demo)");
 	}
 }	
